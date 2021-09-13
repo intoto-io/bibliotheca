3753f017711bd23ac71fdd587f5369c1a05f7bc5
Gaya Kessler <gaya@theclevernode.com>
2021-09-13 14:51:06 +0200

    chore: add storybook, editorconfig, and readme

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "stories": [
+    "../packages/**/*.stories.mdx",
+    "../packages/**/*.stories.@(ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
