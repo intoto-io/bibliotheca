e4bf22f7dbb87b7f78c3576a1f7824bf9aca931e
Gaya Kessler <gaya@theclevernode.com>
2021-09-28 09:34:23 +0200

    feat: expose createa scale helpers

--- packages/graph/helpers/index.ts
+++ packages/graph/helpers/index.ts
@@ -1,3 +1,9 @@
 import { isMissing, isPredicted } from './dataPoint';
+import { createXScale, createYScale } from './createScales';
 
-export { isMissing, isPredicted };
+export {
+  isMissing,
+  isPredicted,
+  createXScale,
+  createYScale,
+};
