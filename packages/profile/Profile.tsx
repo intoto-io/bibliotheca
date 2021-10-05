e4c58c7f6344001f0872048a5b24c4adf41259cf
Gaya Kessler <gaya@theclevernode.com>
2021-10-05 14:23:19 +0200

    feat: ability to provide drawing specifics for ground, stroke, and padding

--- packages/profile/Profile.tsx
+++ packages/profile/Profile.tsx
@@ -5,19 +5,34 @@ import { RiverProfile } from './types';
 
 export interface ProfileProps {
   profile: RiverProfile;
+  strokeWidth?: number;
+  strokeColor?: string;
+  groundFill?: string;
 }
 
-const Profile: FunctionComponent<ProfileProps> = ({ profile }) => {
+const baseLine = line<[number, number]>(([x]) => x, ([_, y]) => y);
+const bankLine = line<[number, number]>(([x]) => x, ([_, y]) => y).curve(curveBasis);
+
+const Profile: FunctionComponent<ProfileProps> = ({
+  profile,
+  strokeColor = 'black',
+  strokeWidth = 1.5,
+  groundFill = '#937960',
+}) => {
   const maxMSL = Math.max(...profile.map((p) => p.msl));
   const minMSL = Math.min(...profile.map((p) => p.msl));
   const width = Math.max(...profile.map((p) => p.x));
   const height = maxMSL - minMSL;
 
-  const padding = 10;
-  const renderWidth = 600;
-  const renderWidthWithoutPadding = renderWidth - (padding * 2);
-  const renderHeight = ((height / width) * renderWidth) + (padding * 2);
-  const ratio = renderWidthWithoutPadding / width;
+  const padding = 5;
+  const bankPadding = 15;
+
+  const totalWidth = 600;
+  const renderWidth = totalWidth - (padding * 2);
+  const renderHeight = ((height / width) * renderWidth);
+  const totalHeight = renderHeight + bankPadding + (padding * 2);
+
+  const ratio = renderWidth / width;
 
   const points = useMemo(() => profile
     .map((p): [number, number] => {
@@ -28,39 +43,31 @@ const Profile: FunctionComponent<ProfileProps> = ({ profile }) => {
       return [x, y];
     }), [height, minMSL, profile, ratio]);
 
-  const closePoints: [number, number][] = [
+  const closePoints: [number, number][] = useMemo(() => [
     // right top
     [points[points.length - 1][0], points[points.length - 1][1]],
     // right bottom
-    [points[points.length - 1][0], (renderHeight - (padding / 2))],
+    [points[points.length - 1][0], renderHeight + padding + bankPadding],
     // left bottom
-    [points[0][0], (renderHeight - (padding / 2))],
+    [points[0][0], renderHeight + padding + bankPadding],
     // left top
     [points[0][0], points[0][1]],
-  ];
+  ], [points, renderHeight]);
 
-  const path = line<[number, number]>(([x]) => x, ([_, y]) => y)
-    .curve(curveBasis)(points);
-
-  const closePath = line<[number, number]>(([x]) => x, ([_, y]) => y)(closePoints);
+  const path = bankLine(points);
+  const closePath = baseLine(closePoints);
 
   return (
     <svg
-      width={renderWidth}
-      height={renderHeight}
-      viewBox={`0 0 ${renderWidth} ${renderHeight}`}
+      width={totalWidth}
+      height={totalHeight}
+      viewBox={`0 0 ${totalWidth} ${totalHeight}`}
     >
       <path
-        d={path || ''}
-        stroke="black"
-        strokeWidth={2}
-        fill="none"
-      />
-      <path
-        d={closePath || ''}
-        stroke="black"
-        strokeWidth={2}
-        fill="none"
+        d={[path, closePath].join(' ') || ''}
+        stroke={strokeColor}
+        strokeWidth={strokeWidth}
+        fill={groundFill}
       />
     </svg>
   );
