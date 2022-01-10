c621bed4e10450c41ad5890cda055a184733860e
Gaya Kessler <gaya@theclevernode.com>
2022-01-10 09:19:56 +0100

    feat: only allow light background

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,18 +1,30 @@
 import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider as Emotion10ThemeProvider } from 'emotion-theming';
 
 import MuiTheme from './MuiTheme';
 
 const withThemeProvider = (Story, context) => {
   return (
-    <ThemeProvider theme={MuiTheme}>
-      <Story {...context} />
-    </ThemeProvider>
+    <Emotion10ThemeProvider theme={MuiTheme}>
+      <ThemeProvider theme={MuiTheme}>
+        <Story {...context} />
+      </ThemeProvider>
+    </Emotion10ThemeProvider>
   );
 };
 
 export const decorators = [withThemeProvider];
 
 export const parameters = {
+  backgrounds: {
+    default: 'Light',
+    values: [
+      {
+        name: 'Light',
+        value: '#fff',
+      },
+    ],
+  },
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
