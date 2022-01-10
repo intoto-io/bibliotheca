c7865f7b4aeb923bfd67a0bce043057d469f27d2
Gaya Kessler <gaya@theclevernode.com>
2022-01-10 09:27:32 +0100

    feat: add default font to storybook for components not using MUI5

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -7,6 +7,15 @@ const withThemeProvider = (Story, context) => {
   return (
     <Emotion10ThemeProvider theme={MuiTheme}>
       <ThemeProvider theme={MuiTheme}>
+        {/* Default font settings for components not using MUI5 */}
+        <style>
+          {`
+            body {
+              font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+              font-size: 15px;      
+            }
+          `}
+        </style>
         <Story {...context} />
       </ThemeProvider>
     </Emotion10ThemeProvider>
