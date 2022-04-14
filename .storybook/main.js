8c139e822f33dcca39b9e2ba5dc2a915a3577681
Gaya Kessler <gaya@theclevernode.com>
2022-04-14 12:01:27 +0200

    chore: remove emotion styled

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,3 @@
-// note: this is needed for MUI v5
-const path = require('path');
-const toPath = (filePath) => path.join(process.cwd(), filePath);
-
 module.exports = {
   stories: [
     '../packages/**/*.stories.mdx',
@@ -13,19 +9,5 @@ module.exports = {
   typescript: {
     check: false,
     reactDocgen: false,
-  },
-  // note: this is needed for MUI v5
-  webpackFinal: async (config) => {
-    return {
-      ...config,
-      resolve: {
-        ...config.resolve,
-        alias: {
-          ...config.resolve.alias,
-          '@emotion/core': toPath('node_modules/@emotion/react'),
-          'emotion-theming': toPath('node_modules/@emotion/react'),
-        },
-      },
-    };
   },
 }
