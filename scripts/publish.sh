5ebc8e0573e0f534cc12c9dcbd8a81328869763c
Gaya Kessler <gaya@theclevernode.com>
2022-05-03 13:32:52 +0200

    chore: bumped packages to version: 0.9.2

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-CURRENT_BRANCH="$(git branch --show-current)"
-if [[ "$CURRENT_BRANCH" == "master" ]]; then
-  source .env
-  npm config set '//registry.npmjs.org/:_authToken' "${NPM_TOKEN}"
-  npm publish --workspaces
-else
-  echo "Aborting publishing because you are not on the master branch."
-fi
-
-
+source .env
+npm config set '//registry.npmjs.org/:_authToken' "${NPM_TOKEN}"
+npm publish --workspaces
