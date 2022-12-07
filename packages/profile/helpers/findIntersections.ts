c5978ed7ba007cb6b0168ff0deaf24d5941275dd
Gaya Kessler <gaya@theclevernode.com>
2022-12-07 11:20:05 +0100

    fix: fix intersection calculations

--- packages/profile/helpers/findIntersections.ts
+++ packages/profile/helpers/findIntersections.ts
@@ -7,8 +7,8 @@ function calculateIntersection(
   p4: ProfilePoint,
 ): ProfilePoint {
   // down part of intersection point formula
-  const d1 = (p1.x - p2.x) * (p3.msl - p4.msl); // (x1 - x2) * (y3 - y4)
-  const d2 = (p1.msl - p2.msl) * (p3.x - p4.x); // (y1 - y2) * (x3 - x4)
+  const d1 = (p1.x - p2.x) * (p3.y - p4.y); // (x1 - x2) * (y3 - y4)
+  const d2 = (p1.y - p2.y) * (p3.x - p4.x); // (y1 - y2) * (x3 - x4)
   const d = (d1) - (d2);
 
   if (d === 0) {
@@ -16,26 +16,26 @@ function calculateIntersection(
   }
 
   // upper part of intersection point formula
-  const u1 = (p1.x * p2.msl - p1.msl * p2.x); // (x1 * y2 - y1 * x2)
-  const u4 = (p3.x * p4.msl - p3.msl * p4.x); // (x3 * y4 - y3 * x4)
+  const u1 = (p1.x * p2.y - p1.y * p2.x); // (x1 * y2 - y1 * x2)
+  const u4 = (p3.x * p4.y - p3.y * p4.x); // (x3 * y4 - y3 * x4)
 
   const u2x = p3.x - p4.x; // (x3 - x4)
   const u3x = p1.x - p2.x; // (x1 - x2)
-  const u2y = p3.msl - p4.msl; // (y3 - y4)
-  const u3y = p1.msl - p2.msl; // (y1 - y2)
+  const u2y = p3.y - p4.y; // (y3 - y4)
+  const u3y = p1.y - p2.y; // (y1 - y2)
 
   // intersection point formula
   const px = (u1 * u2x - u3x * u4) / d;
   const py = (u1 * u2y - u3y * u4) / d;
 
-  return { x: px, msl: py };
+  return { x: px, y: py };
 }
 
 function findIntersections(
   profile: ProfilePoint[],
-  msl?: number,
+  y?: number,
 ): [ProfilePoint, ProfilePoint] | undefined {
-  if (typeof msl === 'undefined') {
+  if (typeof y === 'undefined') {
     return undefined;
   }
 
@@ -44,18 +44,18 @@ function findIntersections(
 
   const leftIndex = leftProfile.findIndex(
     (item, index, allItems) => allItems[index + 1]
-      && item.msl > msl && allItems[index + 1].msl < msl,
+      && item.y > y && allItems[index + 1].y < y,
   );
   const rightIndex = rightProfile.findIndex(
     (item, index, allItems) => allItems[index + 1]
-      && item.msl < msl && allItems[index + 1].msl > msl,
+      && item.y < y && allItems[index + 1].y > y,
   );
 
   if (leftIndex === -1 || rightIndex === -1) {
     return undefined;
   }
 
-  const waterLine = [{ x: 0, msl }, { x: profile[profile.length - 1].x, msl }];
+  const waterLine = [{ x: 0, y }, { x: profile[profile.length - 1].x, y }];
 
   return [
     calculateIntersection(
