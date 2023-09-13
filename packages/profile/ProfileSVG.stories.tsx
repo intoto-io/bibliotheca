b3c3d9c65623c979897f17ee714f99ddbc00b8fa
Richard Egeli <richard.stromland.egeli@intoto.io>
2023-09-13 13:22:27 +0200

    feat: added a ProfileSVG component handling SVGs

---
+++ packages/profile/ProfileSVG.stories.tsx
@@ -0,0 +1,23 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Story, Meta } from "@storybook/react/types-6-0";
+
+import ProfileSVG, { ProfileSVGProps } from "./ProfileSVG";
+import ArtBoard from "./mocks/whereArtboard.svg";
+
+export default {
+  title: "Components/Profile",
+  component: ProfileSVG,
+} as Meta;
+
+const Template: Story<ProfileSVGProps> = function Template(args) {
+  return <ProfileSVG {...args} />;
+};
+
+export const WithDynamicScene = Template.bind({});
+
+WithDynamicScene.args = {
+  waterHeight: 3.5,
+  waterHeightFactor: 28.41,
+  svgPath: ArtBoard,
+  svgString: "",
+};
